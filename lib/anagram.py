597d110b6a98749b35317fd2f1816aa4a64cf9b3
eric <ericmaina11455@gmail.com>
2024-05-27 08:47:20 +0300

    solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,anagram):
+        self.anagram = anagram
+    def test_instantiates_with_word(self):
+        return(self.anagram)
+    def match(self,list):
+        for each_word in list:
+            our_second_sort = sorted(each_word)
+            if our_second_sort == sorted(self.anagram):
+                print(our_second_sort)
+                return
+            else:
+                print("Okay")
+                
+    pass        
+    
+neno = Anagram('google')
+neno.match(['enlists', 'google', 'inlets', 'banana'])
